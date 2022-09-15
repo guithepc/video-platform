01ff6c9f3aaa4aace2286fbef3351f203bbbaba8
Guilherme <guilherme.pc@yahoo.com>
2022-09-14 21:42:27 -0300

    adicionando projeto spring

---
+++ back/videoplatform/src/main/java/com/fmu/br/videoplatform/VideoplatformApplication.java
@@ -0,0 +1,13 @@
+package com.fmu.br.videoplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoplatformApplication.class, args);
+	}
+
+}
---
+++ back/videoplatform/src/test/java/com/fmu/br/videoplatform/VideoplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmu.br.videoplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoplatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
